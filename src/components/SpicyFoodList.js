ebe29bdc51d0bb706edcba1754b81ee26a8a2930
GoldenPavilion <rcorystine@gmail.com>
2022-02-07 20:23:25 -0500

    filters food list based on type of cuisine

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -5,6 +5,14 @@ function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
   const [filterBy, setFilterBy] = useState("All");
 
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All"){
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
+
   function handleAddFood() {
     const newFood = getNewSpicyFood();
     const newFoodArray = [...foods, newFood];
@@ -25,7 +33,11 @@ function SpicyFoodList() {
     setFoods(newFoodArray);
   }
 
-  const foodList = foods.map((food) => (
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+
+  const foodList = foodsToDisplay.map((food) => (
     <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
@@ -33,14 +45,17 @@ function SpicyFoodList() {
 
   return (
     <div>
-      <select  name="filter">
+      <select  name="filter" onChange={handleFilterChange}>
         <option value="All">All</option>
         <option value="American">American</option>
         <option value="Sichuan">Sichuan</option>
         <option value="Thai">Thai</option>
         <option value="Mexican">Mexican</option>
       </select>
+      <br />
+      <br />
       <button onClick={handleAddFood}>Add New Food</button>
+      <br />
       <ul>{foodList}</ul>
     </div>
   );
